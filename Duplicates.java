0a672489fa3528b9ece04b9c7d9189bf3aa8fdf3
Lester Deeley <lester.deeley@yahoo.com>
2020-10-08 12:48:15 +0100

    Duplicates - check for the presence of of duplicate characters in a String and then displays to console. No need to use a char[] and do a sort. removed.

--- Duplicates.java
+++ Duplicates.java
@@ -1,6 +1,5 @@
-import java.util.Arrays;
+
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class Duplicates {
@@ -10,12 +9,13 @@ public class Duplicates {
     public static void main(String[] args) {
         String targetString = args[0];
 
-        char[] targetArray = targetString.toCharArray();
-        Arrays.sort(targetArray);
+        //char[] targetArray = targetString.toCharArray();
+        //Arrays.sort(targetArray);
 
         HashMap<Character,Integer> charCount = new HashMap<>();
 
-        for (char ch : targetArray){
+        for (int i = 0; i < targetString.length(); i++){
+            char ch = targetString.charAt(i);
             if (charCount.containsKey(ch)){
                 int count = charCount.get(ch);
                 count++;
@@ -23,12 +23,11 @@ public class Duplicates {
             } else charCount.put(ch,1);
         }
 
-        for (Map.Entry<Character, Integer> characterIntegerEntry : charCount.entrySet()) {
-            Map.Entry<Character,Integer> mapElement = (Map.Entry) characterIntegerEntry;
-            if ((int) mapElement.getValue() == 2) System.out.println(mapElement.getKey());
+        for (Map.Entry<Character, Integer> mapElement : charCount.entrySet()) {
+            if (mapElement.getValue() == 2) System.out.println(mapElement.getKey());
         }
 
-        
+
     }
 
 }
