1d9a06fd98d3eeda2de3008be19e79cd1d85b9f8
Lester Deeley <lester.deeley@yahoo.com>
2020-10-20 15:09:28 +0100

    String exercise...Reversing the characters in a string, maintaining word order

---
+++ CodePoints.java
@@ -0,0 +1,8 @@
+public class CodePoints {
+
+    public static void main(String[] args) {
+        String str = String.valueOf(Character.toChars(128149));
+        System.out.println(str);
+    }
+
+}
